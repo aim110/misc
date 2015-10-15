dc4383286411707570cb2fc5d1957b07cb238bd9
Ivan Mikhaylyukov <qqq@yandex-team.ru>
2015-10-15 19:56:23 +0300

    virtualenv creation script added

---
+++ get_venv.sh
@@ -0,0 +1,4 @@
+curl -O https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.9.tar.gz
+tar xvfz virtualenv-1.9.tar.gz
+python virtualenv-1.9/virtualenv.py .env && rm -rf virtualenv-1.9.tar.gz virtualenv-1.9
+
